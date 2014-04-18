569323f82997d5a09704cbc28b48ef1540ffeb00
Fr099y <usukhuus@gmail.com>
2014-04-18 13:41:27 +0800

    First commit from Xcode

---
+++ MemoryGame/HomeScreen.h
@@ -0,0 +1,13 @@
+//
+//  HomeScreen.h
+//  MemoryGame
+//
+//  Created by Fr099yz on 4/18/14.
+//  Copyright (c) 2014 Fr099y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeScreen : UIViewController
+
+@end
---
+++ MemoryGame/HomeScreen.m
@@ -0,0 +1,38 @@
+//
+//  HomeScreen.m
+//  MemoryGame
+//
+//  Created by Fr099yz on 4/18/14.
+//  Copyright (c) 2014 Fr099y. All rights reserved.
+//
+
+#import "HomeScreen.h"
+
+@interface HomeScreen ()
+
+@end
+
+@implementation HomeScreen
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
