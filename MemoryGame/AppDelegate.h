b108ff17d4b3b97cc5fea0eb7b82fe6226eff5e9
Fr099y <usukhuus@gmail.com>
2014-04-18 13:28:56 +0800

    Initial Commit

---
+++ MemoryGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MemoryGame
+//
+//  Created by Fr099yz on 4/18/14.
+//  Copyright (c) 2014 Fr099y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MemoryGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MemoryGame
+//
+//  Created by Fr099yz on 4/18/14.
+//  Copyright (c) 2014 Fr099y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
